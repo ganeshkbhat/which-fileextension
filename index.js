bbf5d766bac5b9a107e96b592e0cabd5cb84f288
Ganesh B <ganeshsurfs@gmail.com>
2024-02-27 17:39:03 +0530

    [Commit] First commit for detecting file types using file names

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 
+function detect() {
+  let e = [".html, php, php2, .php3, phpt, .phtml, .phar, .do, .svg, .action, .cgi, .asp, .ashx, cshtml, .js, .xml, .php7, .asmx, .jsp, .cgi, .htm .cer .aspx , .php4, .php5, .odt"]
+  let a = str.split(".")
+  returm(!!(e.includes(a[a.length - 1]) || e.includes(a[a.length - 2])));
+}
