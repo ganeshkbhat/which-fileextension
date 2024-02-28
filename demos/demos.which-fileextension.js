d8fa235b1d03aa4688e92680c446a342d0845fde
Ganesh B <ganeshsurfs@gmail.com>
2024-02-28 19:13:42 +0530

    [Commit] Adding tests and demos files for files. Minor changes to readme

--- demos/demos.which-fileextension.js
+++ demos/demos.which-fileextension.js
@@ -13,3 +13,25 @@
 /* eslint no-console: 0 */
 
 'use strict';
+
+const dr = require("../index.js");
+
+console.log(dr.textExtensions);
+console.log(dr.binaryExtensions);
+dr.detect("test/tester.jpg", dr.binaryExtensions) // true
+dr.detect("test/tester.jpg", dr.textExtensions) // false
+
+
+dr.isText("test/tester.txt") // true
+dr.isBinary("test/tester.exe") // true
+
+
+dr.detect("test/tester.jpg", dr.binaryExtensions) // true
+dr.detect("test/tester.jpg", dr.textExtensions) // false
+dr.detect("test/tester.jpg", ["gz", "txt", "jpg", "jpeg"]) // true`
+dr.isExtension("test/tester.jpg", "jpg") // true
+dr.isExtension("test/tester.jpg", "txt") // false
+dr.isTextExtension("test/tester.txt") // true
+dr.isTextExtension("test/tester.jpeg") // false
+dr.isBinaryExtension("test/tester.jpeg") // true
+
