f2644169f48e7943b5c64e88a2e741ebf8bf98d6
Ganesh B <ganeshsurfs@gmail.com>
2024-02-28 19:08:27 +0530

    [Commit] Adding tests and demos files for files

---
+++ demos/demos.which-fileextension.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/which-fileextension
+ * npmjs Link: https://www.npmjs.com/package/which-fileextension
+ * File: index.js
+ * File Description: 
+ * 
+*/
+/* eslint no-console: 0 */
+
+'use strict';
