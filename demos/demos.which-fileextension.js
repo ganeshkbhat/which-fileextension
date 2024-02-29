447bce69193f1283c2d51be6827b44f48f5529fb
Ganesh B <ganeshsurfs@gmail.com>
2024-02-29 10:14:33 +0530

    [COMMIT] Adding minor changes in doc comments

--- demos/demos.which-fileextension.js
+++ demos/demos.which-fileextension.js
@@ -1,15 +1,17 @@
 /**
  * 
- * Package: 
+ * Package: which-fileextension
  * Author: Ganesh B
  * Description: 
- * Install: npm i  --save
- * Github: https://github.com/ganeshkbhat/which-fileextension
- * npmjs Link: https://www.npmjs.com/package/which-fileextension
+ * Install: npm i which-fileextension --save
+ * Github: https://github.com/ganeshkbhat/which-fileextension/
+ * npmjs Link: https://www.npmjs.com/package/which-fileextension/
  * File: index.js
  * File Description: 
  * 
+ * 
 */
+
 /* eslint no-console: 0 */
 
 'use strict';
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /**
  * 
- * Package: 
+ * Package: which-fileextension
  * Author: Ganesh B
  * Description: 
- * Install: npm i  --save
- * Github: https://github.com/ganeshkbhat/
- * npmjs Link: https://www.npmjs.com/package/
+ * Install: npm i which-fileextension --save
+ * Github: https://github.com/ganeshkbhat/which-fileextension/
+ * npmjs Link: https://www.npmjs.com/package/which-fileextension/
  * File: index.js
  * File Description: 
+ * 
  * 
 */
 
