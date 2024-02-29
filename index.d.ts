e94b7c0444c63ec543e839d77de36d1b0f92dbf2
Ganesh B <ganeshsurfs@gmail.com>
2024-02-29 19:25:45 +0530

    [COMMIT] check if [a] an array or [b] is an typed array or [c] an buffer array or [d] specific type using the package. Adding demos folder to readme. Adding tsc options

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,2 @@
-export function isText(filename: any, buffer: any): boolean | null;
-export function isBinary(filename: any, buffer: any): boolean | null;
-export function detect(str: any, arr: any): boolean;
-export function isExtension(str: any, ext: any): boolean;
-export function isTextExtension(str: any): boolean;
-export function isBinaryExtension(str: any): boolean;
-export const binaryExtensions: string[];
-export const textExtensions: string[];
-export const scriptInjectionExtensions: string[];
+export { exp as default };
 //# sourceMappingURL=index.d.ts.map
